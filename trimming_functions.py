8a7e2e934b76b825a46a1f9afdab247764a16a2b
Joanna Hard <joanna.hard@ki.se>
2016-03-22 23:49:51 +0100

    fixed indentation

--- trimming_functions.py
+++ trimming_functions.py
@@ -7,15 +7,16 @@ class SampleTrimmer():
         for filePairId,readCount,fastq1,fastq2,sampleId in sample.getFastqs():
             output_file = open(sample.scriptPath+'/trimming.'+str(filePairId)+'.sbatch.sh', 'w')
             output = '#! /bin/bash -l'+'\n'
-	    output += '#SBATCH -A '+self.analysispipe.settings.uppmaxProject+'\n'
-	    output += '#SBATCH -n 2 -p node'+'\n'
-	    output += '#SBATCH -t 240:00:00'+'\n'
-	    output += '#SBATCH -J trim.'+sample.name+'.'+str('test')+'\n'
-	    output += '#SBATCH -e '+sample.logPath+'/stderr.trimming.'+sample.name+'.'+str(filePairId)+'.txt'+'\n'
-	    output += '#SBATCH -o '+sample.logPath+'/stdout.trimming.'+sample.name+'.'+str(filePairId)+'.txt'+'\n'
+        output += '#SBATCH -A '+self.analysispipe.settings.uppmaxProject+'\n'
+        output += '#SBATCH -n 2 -p node'+'\n'
+        output += '#SBATCH -t 240:00:00'+'\n'
+        output += '#SBATCH -J trim.'+sample.name+'.'+str('test')+'\n'
+        output += '#SBATCH -e '+sample.logPath+'/stderr.trimming.'+sample.name+'.'+str(filePairId)+'.txt'+'\n'
+        output += '#SBATCH -o '+sample.logPath+'/stdout.trimming.'+sample.name+'.'+str(filePairId)+'.txt'+'\n'
+
         #
         # Go to sample path, load tools and define variables
-	    #
+        #
         output += 'echo "$(date) Running on: $(hostname)"'+'\n'
         output += 'cd '+sample.path+'\n'
         output += 'echo'+'\n'
@@ -27,67 +28,66 @@ class SampleTrimmer():
         # Rubicon trimming
         #
         if not self.analysispipe.settings.skiprubicon:
-		output += ''+self.analysispipe.settings.wga_trimmer+'/wgaAdapterTrimmer.py -i '+r1_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed.fq 2> '+sample.logPath+'/rubiconWgaTrimming.'+str(filePairId)+'.r1.log.txt &\n'
-		output += ''+self.analysispipe.settings.wga_trimmer+'/wgaAdapterTrimmer.py -i '+r2_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed.fq 2> '+sample.logPath+'/rubiconWgaTrimming.'+str(filePairId)+'.r2.log.txt &\n'
-		output += 'wait\n'
-		
+            output += ''+self.analysispipe.settings.wga_trimmer+'/wgaAdapterTrimmer.py -i '+r1_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed.fq 2> '+sample.logPath+'/rubiconWgaTrimming.'+str(filePairId)+'.r1.log.txt &\n'
+            output += ''+self.analysispipe.settings.wga_trimmer+'/wgaAdapterTrimmer.py -i '+r2_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed.fq 2> '+sample.logPath+'/rubiconWgaTrimming.'+str(filePairId)+'.r2.log.txt &\n'
+            output += 'wait\n'
+            
         r1_in = sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed.fq'
         r2_in = sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed.fq'
         #
         # Malbac trimming 
         #
         if not self.analysispipe.settings.skipmalbac:
-		output += '\n'
-		output += 'cutadapt -n 10 -g GTGAGTGATGGTTGAGGTAGTGTGGAG -a CTCCACACTACCTCAACCATCACTCAC '+r1_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed2.fq  2> '+sample.logPath+'/malbacWgaTrimming.'+str(filePairId)+'.r1.log.txt &\n'
-		output += 'cutadapt -n 10 -g GTGAGTGATGGTTGAGGTAGTGTGGAG -a CTCCACACTACCTCAACCATCACTCAC '+r2_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed2.fq  2> '+sample.logPath+'/malbacWgaTrimming.'+str(filePairId)+'.r2.log.txt &\n'
-		output += 'wait\n'
-		output += '\n'
-		
+            output += '\n'
+            output += 'cutadapt -n 10 -g GTGAGTGATGGTTGAGGTAGTGTGGAG -a CTCCACACTACCTCAACCATCACTCAC '+r1_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed2.fq  2> '+sample.logPath+'/malbacWgaTrimming.'+str(filePairId)+'.r1.log.txt &\n'
+            output += 'cutadapt -n 10 -g GTGAGTGATGGTTGAGGTAGTGTGGAG -a CTCCACACTACCTCAACCATCACTCAC '+r2_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed2.fq  2> '+sample.logPath+'/malbacWgaTrimming.'+str(filePairId)+'.r2.log.txt &\n'
+            output += 'wait\n'
+            output += '\n'
         r1_in = sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed2.fq'
         r2_in = sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed2.fq'
-		
+        
         if not self.analysispipe.settings.skiprubicon:output += 'rm -v '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed.fq '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed.fq\n'
         #
         # Ampli1 trimming
         #
         if not self.analysispipe.settings.skipampli1:
-		output += '\n'
-		output += 'cutadapt -n 10 -g AGTGGGATTCCTGCTGTCAGT '+r1_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed3.fq  2> '+sample.logPath+'/ampli1WgaTrimming.'+str(filePairId)+'.r1.log.txt &\n'
-		output += 'cutadapt -n 10 -g AGTGGGATTCCTGCTGTCAGT '+r2_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed3.fq  2> '+sample.logPath+'/ampli1WgaTrimming.'+str(filePairId)+'.r2.log.txt &\n'
-		r1_in = sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed3.fq'
-		r2_in = sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed3.fq'
-		output += 'wait\n'
-		output += '\n'
-		if not self.analysispipe.settings.skiprubicon and self.analysispipe.settings.skipmalbac:
-		    output += 'rm -v '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed.fq '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed.fq\n'
-		if not self.analysispipe.settings.skipmalbac:
-		    output += 'rm -v '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed2.fq '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed2.fq\n'
+            output += '\n'
+            output += 'cutadapt -n 10 -g AGTGGGATTCCTGCTGTCAGT '+r1_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed3.fq  2> '+sample.logPath+'/ampli1WgaTrimming.'+str(filePairId)+'.r1.log.txt &\n'
+            output += 'cutadapt -n 10 -g AGTGGGATTCCTGCTGTCAGT '+r2_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed3.fq  2> '+sample.logPath+'/ampli1WgaTrimming.'+str(filePairId)+'.r2.log.txt &\n'
+            r1_in = sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed3.fq'
+            r2_in = sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed3.fq'
+            output += 'wait\n'
+            output += '\n'
+            if not self.analysispipe.settings.skiprubicon and self.analysispipe.settings.skipmalbac:
+                output += 'rm -v '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed.fq '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed.fq\n'
+            if not self.analysispipe.settings.skipmalbac:
+                output += 'rm -v '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed2.fq '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed2.fq\n'
         #
-	    # Illumina  adapter trimming
-	    #
+        # Illumina  adapter trimming
+        #
         adaptersToTrim = '-a CTGTCTCTTATACACATCTGACGCTGCCGACGA -a CTGTCTCTTATACACATCTCCGAGCCCACGAGAC -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -a AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT'
         output += 'cutadapt -n 3 '+adaptersToTrim+' '+r1_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaAndilluminaTrimmed.fq 2> '+sample.logPath+'/illuminaAndNexteraTrimming.'+str(filePairId)+'.r1.log.txt &\n'
         output += 'cutadapt -n 3 '+adaptersToTrim+' '+r2_in+' > '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaAndilluminaTrimmed.fq 2> '+sample.logPath+'/illuminaAndNexteraTrimming.'+str(filePairId)+'.r2.log.txt &\n'
         output += 'wait\n'
         #
-	    # Remove temporary files
-	    #
+        # Remove temporary files
+        #
         output += 'rm -v '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaTrimmed3.fq '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaTrimmed3.fq \n'
         output += 'wait\n'
         #
-	    # Quality trimmming
-	    #
+        # Quality trimmming
+        #
         output += ''+self.analysispipe.settings.TrimBWA+'/TrimBWAstyle.pl -q 20 '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaAndilluminaTrimmed.fq > '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaIlluminaAndQualityTrimmed.fq 2> '+sample.logPath+'/qualityTrimming.'+str(filePairId)+'.r1.log.txt &\n'
         output += ''+self.analysispipe.settings.TrimBWA+'/TrimBWAstyle.pl -q 20 '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaAndilluminaTrimmed.fq > '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaIlluminaAndQualityTrimmed.fq 2> '+sample.logPath+'/qualityTrimming.'+str(filePairId)+'.r2.log.txt &\n'
         output += 'wait\n'
         #
-	    # Remove temporary files
-	    #
+        # Remove temporary files
+        #
         output += 'rm -v '+sample.tempPath+'/'+str(filePairId)+'.r1.wgaAndilluminaTrimmed.fq '+sample.tempPath+'/'+str(filePairId)+'.r2.wgaAndilluminaTrimmed.fq\n'
         output += 'wait\n'
         #
-	    # Remove empty or "N" only sequences
-	    #
+        # Remove empty or "N" only sequences
+        #
         output += 'python '+self.analysispipe.settings.removeEmptyReads+'/removeEmptyReads.py '
         output += sample.tempPath+'/'+str(filePairId)+'.r1.wgaIlluminaAndQualityTrimmed.fq '
         output += sample.tempPath+'/'+str(filePairId)+'.r2.wgaIlluminaAndQualityTrimmed.fq '
@@ -96,23 +96,23 @@ class SampleTrimmer():
         output += sample.dataPath+'/'+str(filePairId)+'.singletts.fq '
         output += '>&2 2> '+sample.logPath+'/removeEmptyReads.'+str(filePairId)+'.log.txt\n'
         #
-	    # Compress files
-	    #
+        # Compress files
+        #
         output += 'gzip -v9 '+sample.dataPath+'/'+str(filePairId)+'.r1.allTrimmed.fq &\n'
         output += 'gzip -v9 '+sample.dataPath+'/'+str(filePairId)+'.r2.allTrimmed.fq  &\n'
         output += 'gzip -v9 '+sample.dataPath+'/'+str(filePairId)+'.singletts.fq &\n'
         output += 'wait\n'
         #
-	    # FASTQC
-	    #
+        # FASTQC
+        #
         output += 'fastqc '+sample.dataPath+'/'+str(filePairId)+'.r1.allTrimmed.fq.gz &\n'
         output += 'fastqc '+sample.dataPath+'/'+str(filePairId)+'.r2.allTrimmed.fq.gz &\n'
         output += 'fastqc '+sample.dataPath+'/'+str(filePairId)+'.singletts.fq.gz &\n'
         output += 'wait\n'
         output += 'mv -v '+sample.dataPath+'/*fastqc* '+sample.fastqcPath+'/\n'
         #
-	    # Final output and write script to file
-	    #
+        # Final output and write script to file
+        #
         output += 'echo'+'\n'
         output += 'wait'+'\n'
         output += 'echo "$(date) AllDone"'+'\n'
